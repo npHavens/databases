7b4696f9ca771ee0fc23398321098516b89db3a7
Nick Havens <npahavens@gmail.com>
2017-08-07 21:03:05 -0500

    added tables for rooms and users and foreign keys

--- server/schema.sql
+++ server/schema.sql
@@ -3,13 +3,44 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  id INT(5),
-  user INT(25),
-  room INT(25),
+  id INT(5) DEFAULT 1,
+  user INT(25) REFERENCES users(id),
+  room INT(25) REFERENCES rooms(id),
   text VARCHAR(144),
   PRIMARY KEY(id)
-
+  -- FOREIGN KEY(user) REFERENCES users(id)
 );
+
+CREATE TABLE users (
+  id INT(5) DEFAULT 1,
+  name VARCHAR(25),
+  PRIMARY KEY(id)
+);
+
+
+CREATE TABLE rooms (
+  id INT(5) DEFAULT 1,
+  name VARCHAR(25),
+  PRIMARY KEY(id)
+);
+
+
+
+
+/*
+
+CREATE TABLE classes (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  department INTEGER,
+  teacher INTEGER,
+  FOREIGN KEY(department) REFERENCES departments(id),
+  FOREIGN KEY(teacher) REFERENCES teachers(id)
+);
+*/
+
+
+
 
 /* Create other tables and define schemas for them here! */
 
