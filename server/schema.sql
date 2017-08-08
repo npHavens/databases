57e9248ae7192f201dd761bc27319e148998528e
Nick Havens <npahavens@gmail.com>
2017-08-07 20:17:52 -0500

    added fields for messages table

--- server/schema.sql
+++ server/schema.sql
@@ -3,7 +3,12 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INT(5),
+  user INT(25),
+  room INT(25),
+  text VARCHAR(144),
+  PRIMARY KEY(id)
+
 );
 
 /* Create other tables and define schemas for them here! */
